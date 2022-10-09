6d6aa9939b3e6389b91256d09e13b327f0d53f4e
Amit Kumar Jha <77675138+AIM-IT4@users.noreply.github.com>
2022-10-09 12:49:25 +0530

    Update gitprofile.config.js

--- gitprofile.config.js
+++ gitprofile.config.js
@@ -7,7 +7,7 @@ const config = {
     limit: 6, // How many projects to display.
     exclude: {
       forks: false, // Forked projects will not be displayed if set to true.
-      projects: [], // These projects will not be displayed. example: ['my-project1', 'my-project2']
+      projects: ['Create-Beautiful-Financial-Data-Charts-in-R'], // These projects will not be displayed. example: ['my-project1', 'my-project2']
     },
   },
   social: {
